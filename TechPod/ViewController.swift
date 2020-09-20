fa65b9bc70ac24a53b2082214ec683fbc84004fc
tatsuomi <tatsu20000303@gmail.com>
2020-09-21 00:16:33 +0900

    TechPod作成途中，テーブルビューまで行った．

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    
+    @IBOutlet var table: UITableView!
+    
+    var songNameArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        table.dataSource = self
     }
-
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //ID付きのセルを収得して，セル付属のtextlabelに”テスト”と表示させてみる．
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+    }
+    
+    
+    
+    
 
 }
 
